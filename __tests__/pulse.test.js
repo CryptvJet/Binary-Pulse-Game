6945849870a36fede83c56013d7e1862b431b9de
cryptvjet <noldjoshua@gmail.com>
2025-07-06 21:33:49 -0400

    Add z parameter for pulses

--- __tests__/pulse.test.js
+++ __tests__/pulse.test.js
@@ -8,7 +8,7 @@ describe('pulse mechanics', () => {
 
   test('pulse moves and toggles cell', () => {
     const grid = initializeGrid(1, 3, 3);
-    launchPulse(0, 0, 1, 0, 1);
+    launchPulse(0, 0, 0, 1, 0, 1);
     updatePulse(1, grid[0]);
     expect(grid[0][0][1].value).toBe(1);
     const pulses = getPulses();
@@ -17,7 +17,7 @@ describe('pulse mechanics', () => {
 
   test('launchPulse stores color', () => {
     const grid = initializeGrid(1, 1, 1);
-    launchPulse(0, 0, 1, 0, 1, 0, '#123456');
+    launchPulse(0, 0, 0, 1, 0, 1, 0, '#123456');
     const pulses = getPulses();
     expect(pulses[0].color).toBe('#123456');
   });
@@ -25,7 +25,7 @@ describe('pulse mechanics', () => {
   test('rapid fire launches multiple pulses', () => {
     const grid = initializeGrid(1, 1, 1);
     for (let i = 0; i < 5; i++) {
-      launchPulse(0, 0, 1, 0, 1);
+      launchPulse(0, 0, 0, 1, 0, 1);
     }
     const pulses = getPulses();
     expect(pulses.length).toBe(5);
